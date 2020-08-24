f58e2bc6f8afd307f3eeb2a25ee68744ad6105b3
Gabriel Zarpelon <zarpelon091098@gmail.com>
2020-08-24 12:28:45 -0300

    default strenght

--- models/Dwarf.js
+++ models/Dwarf.js
@@ -4,7 +4,7 @@ const Dwarf = new mongoose.Schema({
     name: { type: String, required: true },
     userId: { type: String, required: true, unique: true},
     money: {type: Number, default: 0},
-    strength: {type: Number, default: 1},
+    strength: {type: Number, default: 3},
     stamina: {type: Number, default: 3},
     consumables:  [{ type: mongoose.Schema.Types.ObjectId, ref: "Consumable"}],
     equipments:  [{ type: mongoose.Schema.Types.ObjectId, ref: "Equipment"}],
